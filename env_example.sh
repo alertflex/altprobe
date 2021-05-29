b098bf88df5f953a6a635a7f952b5fd5248c0036
olegzhr <14097456+olegzhr@users.noreply.github.com>
2021-05-29 10:43:03 +0200

    Update env_example.sh

--- env_example.sh
+++ env_example.sh
@@ -7,33 +7,33 @@
 # collector
 # work directory and node id and probe id
 export INSTALL_PATH=/home/alertflex/altprobe
-export PROJECT_ID=1af11399-0cef-45be-b579-ef6e580a7bf2
-export NODE_ID=collr
-export PROBE_ID=master
+export PROJECT_ID=0ca7e3b3-1a0c-407a-957d-aab039165217
+export NODE_ID=node01
+export PROBE_ID=collr01
 
 # controller
 # url: "ssl://host:61617" or "tcp://host:61616"
-export AMQ_URL='ssl:\//\af-ctrl:61617'
-export AMQ_USER=user1
+export AMQ_URL='ssl:\/\/af-ctrl:61617'
+export AMQ_USER=admin
 export AMQ_PWD=Password1234
 export AMQ_CERT='\/etc\/altprobe\/Broker.pem'
 export CERT_VERIFY=true
-export AMQ_KEY='\/etc\/altprobe\/Client.pem'
-export KEY_PWD=Password1234
+export AMQ_KEY=indef
+export KEY_PWD=indef
 
 # sources:
 # if *_LOG is "indef", redis connection will use
-export FALCO_LOG='\/var\/log\/falco.json'
-export MODSEC_LOG='\/var\/log\/nginx\/error.log'
+export FALCO_LOG=indef
+export MODSEC_LOG=indef
 export SURI_LOG='\/var\/log\/suricata\/eve.json'
 export WAZUH_LOG='\/var\/ossec\/logs\/alerts\/alerts.json'
 
 # install add-on packages
 export INSTALL_REDIS=yes
 export REDIS_HOST=127.0.0.1
-export INSTALL_FALCO=yes
+export INSTALL_FALCO=no
 export INSTALL_SURICATA=yes
-export SURICATA_INTERFACE=enp0s3
+export SURICATA_INTERFACE=enp0s8
 export INSTALL_WAZUH=yes
 export WAZUH_HOST=127.0.0.1
 export WAZUH_USER=wazuh
