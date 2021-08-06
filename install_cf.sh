d23504505a75c93e7c9754787794b67e2bdd3050
olegzhr <olegzhr@gmail.com>
2021-08-06 16:06:05 +0300

    add install script for cloudformation

---
+++ install_cf.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+
+#load technical project data for Alertflex collector
+source ./env_ami.sh
+
+export INSTALL_PATH=/home/ec2-user/altprobe
+export NODE_ID=node01
+export PROBE_ID=probe01
+export AMQ_URL='tcp:\/\/127.0.0.1:61616'
+export AMQ_USER=admin
+export AMQ_CERT=indef
+export CERT_VERIFY=false
+export AMQ_KEY=indef
+export KEY_PWD=indef
+export FALCO_LOG=indef
+export MODSEC_LOG=indef
+export SURI_LOG=indef
+export WAZUH_LOG='\/var\/ossec\/logs\/alerts\/alerts.json'
+export INSTALL_REDIS=yes
+export REDIS_HOST=127.0.0.1
+export INSTALL_WAZUH=yes
+export WAZUH_HOST=127.0.0.1
+export WAZUH_USER=wazuh
+export WAZUH_PWD=wazuh
+
+
+CURRENT_PATH=`pwd`
+if [[ $INSTALL_PATH != $CURRENT_PATH ]]
+then
+    echo "Please change install directory"
+    exit 0
+fi
+
+echo "*** Installation alertflex collector started***"
+
+sudo yum -y update
+
+echo "*** installation activemq ***"
+sudo yum -y install hiredis hiredis-devel activemq-cpp.x86_64 activemq-cpp-devel.x86_64
+sudo curl -L -O "https://github.com/alertflex/altprobe/releases/download/v1.0.1/altprobe-1.0-1.el7.x86_64.rpm"
+sudo rpm -i altprobe-1.0-1.el7.x86_64.rpm
+
+sudo sed -i "s/_project_id/$PROJECT_ID/g" /etc/altprobe/filters.json
+sudo sed -i "s/_node_id/$NODE_ID/g" /etc/altprobe/altprobe.yaml
+sudo sed -i "s/_probe_id/$PROBE_ID/g" /etc/altprobe/altprobe.yaml
+sudo sed -i "s/_redis_host/$REDIS_HOST/g" /etc/altprobe/altprobe.yaml
+sudo sed -i "s/_wazuh_host/$WAZUH_HOST/g" /etc/altprobe/altprobe.yaml
+sudo sed -i "s/_wazuh_user/$WAZUH_USER/g" /etc/altprobe/altprobe.yaml
+sudo sed -i "s/_wazuh_pwd/$WAZUH_PWD/g" /etc/altprobe/altprobe.yaml
+sudo sed -i "s/_amq_url/$AMQ_URL/g" /etc/altprobe/altprobe.yaml
+sudo sed -i "s/_amq_user/$AMQ_USER/g" /etc/altprobe/altprobe.yaml
+sudo sed -i "s/_amq_pwd/$AMQ_PWD/g" /etc/altprobe/altprobe.yaml
+sudo sed -i "s/_amq_cert/$AMQ_CERT/g" /etc/altprobe/altprobe.yaml
+sudo sed -i "s/_cert_verify/$CERT_VERIFY/g" /etc/altprobe/altprobe.yaml
+sudo sed -i "s/_amq_key/$AMQ_KEY/g" /etc/altprobe/altprobe.yaml
+sudo sed -i "s/_key_pwd/$KEY_PWD/g" /etc/altprobe/altprobe.yaml
+sudo sed -i "s/_falco_log/$FALCO_LOG/g" /etc/altprobe/altprobe.yaml
+sudo sed -i "s/_modsec_log/$MODSEC_LOG/g" /etc/altprobe/altprobe.yaml
+sudo sed -i "s/_suri_log/$SURI_LOG/g" /etc/altprobe/altprobe.yaml
+sudo sed -i "s/_wazuh_log/$WAZUH_LOG/g" /etc/altprobe/altprobe.yaml
+
+sudo chmod go-rwx /etc/altprobe/altprobe.yaml
+
+sudo ln -s /usr/local/bin/altprobe /usr/sbin/altprobe
+sudo ln -s /usr/local/bin/altprobe-restart /usr/sbin/altprobe-restart
+sudo ln -s /usr/local/bin/altprobe-start /usr/sbin/altprobe-start
+sudo ln -s /usr/local/bin/altprobe-status /usr/sbin/altprobe-status
+sudo ln -s /usr/local/bin/altprobe-stop /usr/sbin/altprobe-stop
+
+if [[ $INSTALL_REDIS == yes ]]
+then
+	echo "*** installation redis ***"
+	sudo yum -y install redis 
+	sudo systemctl enable redis
+	sudo systemctl start redis
+fi
+
+if [[ $INSTALL_WAZUH == yes ]]
+then
+
+	echo "*** installation OSSEC/WAZUH server ***"
+	sudo bash -c 'cat > /etc/yum.repos.d/wazuh.repo <<\EOF
+[wazuh_repo]
+gpgcheck=1
+gpgkey=https://packages.wazuh.com/key/GPG-KEY-WAZUH
+enabled=1
+name=Wazuh repository
+baseurl=https://packages.wazuh.com/4.x/yum/
+protect=1
+EOF'
+    sudo yum -y install wazuh-manager
+	
+	sudo sed -i "s/_wazuh_user/$WAZUH_USER/g" /etc/altprobe/altprobe.yaml
+	sudo sed -i "s/_wazuh_pwd/$WAZUH_PWD/g" /etc/altprobe/altprobe.yaml
+	
+	sudo bash -c 'cat << EOF > /var/ossec/api/configuration/api.yaml
+host: 127.0.0.1
+
+https:
+  enabled: no
+  
+# Enable remote commands
+remote_commands:
+  localfile:
+    enabled: yes
+    exceptions: []
+EOF'
+	sudo systemctl enable wazuh-manager
+	sudo systemctl start wazuh-manager
+
+	sudo bash -c 'cat << EOF > /etc/systemd/system/altprobe.service
+[Unit]
+Description=Altprobe
+After=wazuh-manager.service
+[Service]
+Type=forking
+User=root
+ExecStart=/usr/local/bin/altprobe start
+ExecStop=/usr/local/bin/altprobe stop
+ExecReload=/usr/local/bin/altprobe-restart
+PIDFile=/var/run/altprobe.pid
+Restart=on-failure
+RestartSec=30s
+[Install]
+WantedBy=multi-user.target
+EOF'
+else
+	sudo bash -c 'cat << EOF > /etc/systemd/system/altprobe.service
+[Unit]
+Description=Altprobe
+After=network-online.target
+[Service]
+Type=forking
+User=root
+ExecStart=/usr/local/bin/altprobe start
+ExecStop=/usr/local/bin/altprobe stop
+ExecReload=/usr/local/bin/altprobe-restart
+PIDFile=/var/run/altprobe.pid
+Restart=on-failure
+RestartSec=30s
+[Install]
+WantedBy=multi-user.target
+EOF'
+fi
+
+sudo systemctl daemon-reload
+sudo systemctl enable altprobe.service
+sudo systemctl start altprobe.service
+
+exit 0
+
+
+
+
+
+
