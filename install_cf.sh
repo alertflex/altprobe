536e5d5b8a7d60e6d06d737e15ae7f3edd3122b1
olegzhr <olegzhr@gmail.com>
2021-08-09 15:34:50 +0300

    add hiredis

--- install_cf.sh
+++ install_cf.sh
@@ -16,8 +16,6 @@ export FALCO_LOG=indef
 export MODSEC_LOG=indef
 export SURI_LOG=indef
 export WAZUH_LOG='\/var\/ossec\/logs\/alerts\/alerts.json'
-export INSTALL_REDIS=yes
-export REDIS_HOST=127.0.0.1
 export INSTALL_WAZUH=yes
 export WAZUH_HOST=127.0.0.1
 export WAZUH_USER=wazuh
@@ -36,7 +34,7 @@ echo "*** Installation alertflex collector started***"
 sudo yum -y update
 
 echo "*** installation activemq ***"
-sudo yum -y install hiredis hiredis-devel
+sudo yum -y install apr-devel redis hiredis hiredis-devel
 sudo curl -L -O "https://github.com/alertflex/altprobe/releases/download/v1.0.1/altprobe-1.0-1.amzn2.x86_64.rpm"
 sudo rpm -i altprobe-1.0-1.amzn2.x86_64.rpm
 
@@ -67,14 +65,9 @@ sudo ln -s /usr/sbin/altprobe-start /usr/local/bin/altprobe-start
 sudo ln -s /usr/sbin/altprobe-status /usr/local/bin/altprobe-status
 sudo ln -s /usr/sbin/altprobe-stop /usr/local/bin/altprobe-stop 
 
-if [[ $INSTALL_REDIS == yes ]]
-then
-	echo "*** installation redis ***"
-	sudo yum -y install redis
-	sudo sed -i "s/bind 127.0.0.1/bind 0.0.0.0/g" /etc/redis.conf
-	sudo systemctl enable redis
-	sudo systemctl start redis
-fi
+sudo sed -i "s/bind 127.0.0.1/bind 0.0.0.0/g" /etc/redis.conf
+sudo systemctl enable redis
+sudo systemctl start redis
 
 if [[ $INSTALL_WAZUH == yes ]]
 then
