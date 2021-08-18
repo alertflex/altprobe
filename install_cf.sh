f5aa1ae5c8f55e4de10634b1ebb44f61a6c3a28d
olegzhr <14097456+olegzhr@users.noreply.github.com>
2021-08-18 21:42:17 +0300

    Update install_cf.sh

--- install_cf.sh
+++ install_cf.sh
@@ -22,6 +22,7 @@ export WAZUH_PWD=wazuh
 export INSTALL_SURICATA=yes
 export SURI_LOG='\/var\/log\/suricata\/eve.json'
 export SURICATA_INTERFACE=eth1
+export INSTALL_REDIS=yes
 
 
 CURRENT_PATH=`pwd`
@@ -36,7 +37,17 @@ echo "*** Installation alertflex collector started***"
 sudo yum -y update
 
 echo "*** installation activemq ***"
-sudo yum -y install apr-devel redis boost-devel boost-thread
+sudo yum -y install apr-devel boost-devel boost-thread
+
+if [[ $INSTALL_REDIS == yes ]]
+then
+     echo "*** installation redis ***"
+     sudo yum -y install redis 
+     sudo sed -i "s/bind 127.0.0.1/bind 0.0.0.0/g" /etc/redis.conf
+     sudo systemctl enable redis
+     sudo systemctl start redis
+fi
+
 sudo curl -L -O "https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/h/hiredis-0.12.1-2.el7.x86_64.rpm"
 sudo rpm -i hiredis-0.12.1-2.el7.x86_64.rpm
 sudo curl -L -O "https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/h/hiredis-devel-0.12.1-2.el7.x86_64.rpm"
@@ -44,6 +55,7 @@ sudo rpm -i hiredis-devel-0.12.1-2.el7.x86_64.rpm
 sudo curl -L -O "https://github.com/alertflex/altprobe/releases/download/v1.0.1/altprobe-1.0-1.amzn2.x86_64.rpm"
 sudo rpm -i altprobe-1.0-1.amzn2.x86_64.rpm
 sudo ldconfig
+sudo yum -y update
 
 sudo sed -i "s/_project_id/$PROJECT_ID/g" /etc/altprobe/filters.json
 sudo sed -i "s/_node_id/$NODE_ID/g" /etc/altprobe/altprobe.yaml
@@ -72,10 +84,6 @@ sudo ln -s /usr/sbin/altprobe-start /usr/local/bin/altprobe-start
 sudo ln -s /usr/sbin/altprobe-status /usr/local/bin/altprobe-status
 sudo ln -s /usr/sbin/altprobe-stop /usr/local/bin/altprobe-stop 
 
-sudo sed -i "s/bind 127.0.0.1 -::1/bind 0.0.0.0/g" /etc/redis/redis.conf
-sudo systemctl enable redis
-sudo systemctl start redis
-
 if [[ $INSTALL_SURICATA == yes ]]
 then
     echo "*** installation suricata ***"
@@ -101,6 +109,7 @@ WantedBy=multi-user.target
 EOF'
     sudo sed -i "s/_monitoring_interface/$SURICATA_INTERFACE/g" /etc/systemd/system/suricata.service
     sudo systemctl enable suricata
+    sudo systemctl start suricata
 fi
 
 if [[ $INSTALL_WAZUH == yes ]]
