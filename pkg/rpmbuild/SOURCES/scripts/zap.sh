67fedaf0d771a52fd48765908b7416b5e4ec3720
Oleg Zh <olegzhr@gmail.com>
2021-10-26 15:39:46 +0300

    update scripts for dc and zap

--- pkg/rpmbuild/SOURCES/scripts/zap.sh
+++ pkg/rpmbuild/SOURCES/scripts/zap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /root/reports && docker run -v $(pwd):/zap/wrk/:rw -u $UID:$UID -t owasp/zap2docker-stable zap-baseline.py -t $1 -J zap.json -m 1
+cd /root/reports && docker run -v $(pwd):/zap/wrk/:rw -u $UID:$UID -t owasp/zap2docker-stable zap-baseline.py -t $1 -J zap-report.json -m 1
 
 logger "altprobe: run of zap.sh"
